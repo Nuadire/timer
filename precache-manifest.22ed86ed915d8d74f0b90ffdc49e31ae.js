238491f879d896a04d393a9c300ceaf73f98e286
Roman <Nuadire@gmail.com>
2020-06-17 20:51:05 +0500

    Updates

---
+++ precache-manifest.22ed86ed915d8d74f0b90ffdc49e31ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b96bd185487c7e2173ee73d1fd7b8fe",
+    "url": "/timer/index.html"
+  },
+  {
+    "revision": "3935321db4f809bbd7f7",
+    "url": "/timer/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "85f8b31261e79cd42a1e",
+    "url": "/timer/static/css/main.74a5f6e2.chunk.css"
+  },
+  {
+    "revision": "3935321db4f809bbd7f7",
+    "url": "/timer/static/js/2.737943cc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/timer/static/js/2.737943cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85f8b31261e79cd42a1e",
+    "url": "/timer/static/js/main.aa483a63.chunk.js"
+  },
+  {
+    "revision": "ce71648db51885e81543",
+    "url": "/timer/static/js/runtime-main.c557f2c6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.23dac3c25e4d5bdcfd000d52962225f6.js"
+  "/timer/precache-manifest.22ed86ed915d8d74f0b90ffdc49e31ae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/timer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
